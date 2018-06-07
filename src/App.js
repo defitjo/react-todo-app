dca8422e5ca533232f838bac9ad1092782b1468e
Devon <defitjo16@gmail.com>
2018-06-07 01:19:37 -0400

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import RemoveTodo from './RemoveTodo';
+import NewTodo from './NewTodo';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: ['Learn Node.js', 'Learn Vue.js', 'Learn React'],
+    };
+    this.onAdd = this.onAdd.bind(this);
+    this.onRemove = this.onRemove.bind(this);
+  }
+  onAdd(todo) {
+    const newTodos = this.state.todoList.slice();
+    newTodos.push(todo);
+    this.setState({
+      todoList: newTodos,
+    });
+  }
+  onRemove(todo) {
+    const newTodos = this.state.todoList.filter((item) => {
+      return todo !== item;
+    });
+    this.setState({
+      todoList: newTodos,
+    });
+  }
   render() {
+    let { todoList } = this.state;
+    todoList = todoList.map((todo) => {
+      return (
+        <RemoveTodo todo={todo} key={todo.toString()} onRemove={this.onRemove} />
+      );
+    });
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +42,8 @@ class App extends Component {
           <h1 className="App-title">React Todo App</h1>
         </header>
         <section className="Todo-App">
-
+          <NewTodo onAdd={this.onAdd} />
+          <ul>{ todoList }</ul>
         </section>
       </div>
     );
---
+++ src/RemoveTodo.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class RemoveTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleDelete() {
+    this.props.onRemove(this.props.todo);
+  }
+  render() {
+    return (
+      <li>
+        <div className="todo-list">
+          <p className="todo-name">{ this.props.todo }</p>
+          <span className="delete" onClick={this.handleDelete}> Remove </span>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default RemoveTodo;
